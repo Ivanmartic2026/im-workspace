6e5e5be074e7c6c9feb9328f18e439a422accb3f
IM Vision Group AB <imvisiongroupab@gmail.com>
2026-01-07 19:49:19 +0000

    File changes

--- functions/syncGPSTrips.ts
+++ functions/syncGPSTrips.ts
@@ -46,9 +46,14 @@ Deno.serve(async (req) => {
     // Försök att hitta förare baserat på fordonstilldelning
     let defaultDriver = null;
     if (vehicleData.assigned_driver) {
-      const drivers = await base44.asServiceRole.entities.User.filter({ email: vehicleData.assigned_driver });
-      if (drivers && drivers.length > 0) {
-        defaultDriver = drivers[0];
+      try {
+        const allUsers = await base44.asServiceRole.entities.User.list();
+        const driver = allUsers.find(u => u.email === vehicleData.assigned_driver);
+        if (driver) {
+          defaultDriver = driver;
+        }
+      } catch (e) {
+        console.log('Could not fetch users:', e.message);
       }
     }
 
